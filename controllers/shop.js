75525cb292947139b9f936e9f8a8bd635d5f1477
Akshayshah47 <removeit61@gmail.com>
2023-06-16 09:38:16 +0530

    added product detail page

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,15 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req,res,next)=>{
+  const prodId=req.params.productId;
+  Product.fetchProduct(prodId,product=>{
+    console.log("found:");
+    res.render('shop/product-detail',{product: product, pageTitle: product.title, path: '/products'});
+    
+  })
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
